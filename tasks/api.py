808c20d66fa6879a8b44de1f014e62170000c827
leov1eira <leonardovieirabfh@gmail.com>
2019-11-13 16:05:24 -0300

    Add files via upload

---
+++ tasks/api.py
@@ -0,0 +1,52 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+# Algumas tarefas criadas para teste
+tasks = [
+
+]
+
+
+@app.route('/api/tasks')
+def list():
+
+    return jsonify(tasks), 200
+
+
+@app.route('/api/tasks', methods=['POST'])
+def create_task():
+    task = request.get_json()
+    task['id'] = len(tasks)
+    tasks.append(task)
+    return jsonify(task), 201
+
+
+@app.route('/api/tasks/<int:id>', methods=['PUT'])
+def done_task(id):
+
+    for task in tasks:
+        if task['id'] == id:
+            done = request.get_json('done')
+            task['done'] = done['done']
+            return jsonify(task), 200
+
+    return jsonify({'error': 'not found'}), 404
+
+
+@app.route('/tasks/<int:id>', methods=['DELETE'])
+def delete_task(id):
+
+    for task in tasks:
+        if task['id'] == id:
+            del tasks[id]
+            return jsonify(tasks), 204
+
+    return jsonify({'error': 'not found'}), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+print(tasks[0])
